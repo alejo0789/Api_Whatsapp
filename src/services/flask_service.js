398e971d8c0a3fe8ab5936b03aca8d77d06b2ada
alejo0789 <53980553+alejo0789@users.noreply.github.com>
2023-02-16 17:22:14 -0500

    flask service 2

--- src/services/flask_service.js
+++ src/services/flask_service.js
@@ -1,14 +1,18 @@
 const axios = require('axios');
 
-const data = {
-    text: 'prueba',
-   
-};
 
+function sendMessageflask(texto){
+    const data = {
+        text: texto,
+       
+    };
 axios.post('https://f-server2.onrender.com/register', data)
     .then(response => {
         console.log(response.data);
     })
     .catch(error => {
         console.log(error.response.data);
-    });
+    }); }
+
+
+module.exports={sendMessageflask}
