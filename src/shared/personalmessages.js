c3ce8773c6fc1cf6dd238100a96f9db0cea3a6c4
alejo0789 <53980553+alejo0789@users.noreply.github.com>
2023-03-12 11:29:10 -0500

    save messages

--- src/shared/personalmessages.js
+++ src/shared/personalmessages.js
@@ -34,9 +34,32 @@ function saludoControlGastos() {
     // Devuelve el mensaje de despedida aleatorio correspondiente al índice generado
     return goodbyeMessage;
   }
+   function generatesaveinformation() {
+    const messages_save = [
+      "¡Guardado exitoso! ¡Sigue así y mantén el control de tus finanzas!",
+      "¡Excelente! La información se ha guardado. Continúa haciendo un seguimiento de tus finanzas para lograr tus metas financieras.",
+      "¡Muy bien! Los datos se han guardado. Recuerda continuar monitoreando tus finanzas para tomar decisiones financieras informadas.",
+      "¡Hecho! Los datos se han almacenado correctamente. No te olvides de llevar un seguimiento de tus finanzas para mejorar tu situación financiera.",
+      "¡Perfecto! La información se ha guardado satisfactoriamente. Sigue monitoreando y controlando tus finanzas para lograr la estabilidad financiera que deseas.",
+      "¡Listo! Los datos han sido guardados. Mantén el control de tus finanzas y alcanza tus metas financieras.",
+      "¡Genial! Seguimiento iniciado con éxito. Continúa controlando tus finanzas para lograr la libertad financiera que buscas.",
+      "¡Guardado! ¡Sigue así y alcanza tus objetivos financieros!",
+      "¡Listo! ¡Continúa monitoreando tus finanzas para tomar decisiones acertadas!",
+      "¡Hecho! ¡Sigue controlando tus gastos para lograr la estabilidad financiera!",
+      "¡Excelente! ¡Toma el control de tus finanzas y alcanza la libertad financiera!",
+      "¡Genial! ¡La información se ha guardado! ¡Sigue el seguimiento y toma el control de tus finanzas!",
+      "¡Listo! ¡Los datos se han guardado correctamente! ¡Continúa monitoreando tus finanzas para lograr tus metas!",
+      "¡Perfecto! ¡La información ha sido almacenada! ¡No dejes de controlar tus finanzas para mejorar tu situación financiera!"
+    ];
   
+    // Genera un número aleatorio entre 0 y la longitud del array de mensajes de despedida
+    const save_message = messages_save[Math.floor(Math.random() * messages_save.length)];
+  
+    // Devuelve el mensaje de despedida aleatorio correspondiente al índice generado
+    return save_message ;
+  }
 
 
 
 
-  module.exports={saludoControlGastos, generateGoodbyeMessage}
+  module.exports={saludoControlGastos, generateGoodbyeMessage, generatesaveinformation}
