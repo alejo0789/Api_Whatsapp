8bb207598e2f4b094bf237714ed6f376dc05b0da
Alejandro <alejo0789@hotmail.com>
2023-10-28 11:24:26 -0500

    added image bienvenida

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const apiRouter=require("./routes/routes")
-
+const fs = require('fs');
+const path = require('path');
 const app = express();
 
 const PORT=process.env.PORT || 3000;
@@ -14,6 +15,31 @@ app.use("/public",express.static(__dirname + "/images", (err) => {
   
 app.use("/images", express.static(__dirname + "/images"));
 console.log(__dirname)
+//pruebas
+const directoryPath = path.join(__dirname, 'images');
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file); 
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
 app.use("/whatsapp", apiRouter)
 
 
