42950ba6f13a45ab0e4df9bbdff136fa69b3c134
Alejandro <alejo0789@hotmail.com>
2023-10-28 10:52:52 -0500

    added image bienvenida

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ const app = express();
 const PORT=process.env.PORT || 3000;
 
 app.use(express.json())
-app.use(express.static('assets', (err) => {
+app.use(express.static('public', (err) => {
     if (err) {
       console.error('Error serving static files:', err);
     }
   }));
+app.use('/images', express.static('images'));
 app.use("/whatsapp", apiRouter)
 
 
